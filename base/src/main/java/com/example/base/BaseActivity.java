6a03354e5afcd2fdd121a4ef2413f4027032d561
huangdou <huangdou@2dfire.com>
2017-09-29 12:57:08 +0800

    统一管理依赖库

--- base/src/main/java/com/example/base/BaseActivity.java
+++ base/src/main/java/com/example/base/BaseActivity.java
@@ -1,12 +1,10 @@
 package com.example.base;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by chaos
@@ -17,47 +15,15 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     protected Context context;
 
+    private ViewGroup mRootContentView;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(getContentView());
+        setContentView(R.layout.activity_base_container_layout);
 
 
-
-    }
-
-    protected View getContentView(){
-        return LayoutInflater.from(this).inflate(setLayoutId(),null);
-    }
-
-
-    public abstract int setLayoutId();
-
-
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-    }
-
-
-    protected void navigate(){
-
-    }
-
-    protected void navigateWithResult(){
 
     }
 
