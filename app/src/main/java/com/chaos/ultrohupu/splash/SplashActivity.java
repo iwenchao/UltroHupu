afdd2450c1a52fbd2e490c719c333e957848d2e4
iwenchao <iwenchaos@gmail.com>
2017-10-09 18:51:34 +0800

    增加启动页

---
+++ app/src/main/java/com/chaos/ultrohupu/splash/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.chaos.ultrohupu.splash;
+
+import android.view.View;
+
+import com.chaos.base.AbstractBaseActivity;
+
+/**
+ * Created by huangdou
+ * on 2017/10/9.
+ */
+
+public class SplashActivity extends AbstractBaseActivity {
+
+
+    @Override
+    protected void initUILay(View view) {
+
+    }
+
+    @Override
+    protected void loadInitDta() {
+
+    }
+}
